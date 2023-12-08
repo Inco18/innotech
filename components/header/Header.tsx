d72fe962022fd0adcc0802c699c53237217c7f2a
Inco18 <patrykos190@gmail.com>
2023-12-08 10:42:04 +0100

    Add help and contact and adjust header for small screens.

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -6,10 +6,16 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="w-full max-w-screen-2xl flex justify-center items-center gap-4 p-1 ">
-      <Link href={"/"} className="flex items-center gap-2">
-        <Image src={"/logo.png"} height={75} width={75} alt="logo" />
-        <h1 className="font-semibold text-xl">InnoTech</h1>
+    <header className="w-full grid grid-cols-2 grid-rows-2 justify-center items-center lg:px-32 md:flex md:justify-center md:items-center md:gap-4 p-1 max-w-[110rem] ">
+      <Link href={"/"} className="flex items-center gap-2 order-1 mr-auto">
+        <Image
+          src={"/logo.png"}
+          height={65}
+          width={80}
+          alt="logo"
+          className="w-[48px] h-[39px] md:w-[80px] md:h-[65px]"
+        />
+        <h1 className="font-semibold text-md md:text-xl">InnoTech</h1>
       </Link>
       <Search />
       <Help />
--- components/header/HelpButton.tsx
+++ components/header/HelpButton.tsx
@@ -1,23 +1,33 @@
 "use client";
 
+import useWindowDimensions from "@/hooks/useWindowSize";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { PiHeadset } from "react-icons/pi";
+import HelpModal from "./HelpModal";
 
 const HelpButton = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { width } = useWindowDimensions();
+
   const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    if (width > 768) return;
     e.preventDefault();
+    setIsModalOpen(true);
   };
 
   return (
-    <Link
-      href={"/help"}
-      className="p-1 flex flex-col items-center rounded-t-md group-hover:shadow-[0_0_5px_0px_rgba(0,0,0,0.2)]"
-      onClick={handleClick}
-    >
-      <PiHeadset className="text-2xl" />
-      <p className="text-xs">Help and contact</p>
-    </Link>
+    <>
+      <Link
+        href={"/help"}
+        className="p-1 ml-auto flex flex-col w-fit items-center rounded-md md:rounded-t-md hover:shadow-[0_0_5px_0px_rgba(0,0,0,0.2)] md:group-hover:shadow-[0_0_5px_0px_rgba(0,0,0,0.2)]"
+        onClick={handleClick}
+      >
+        <PiHeadset className="text-2xl" />
+        <p className="text-xs">Help and contact</p>
+      </Link>
+      <HelpModal open={isModalOpen} onClose={() => setIsModalOpen(false)} />
+    </>
   );
 };
 
