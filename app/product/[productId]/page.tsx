c2c28245516b9d8ec060f3353bd403492dea4331
Inco18 <patrykos190@gmail.com>
2023-12-11 14:52:30 +0100

    Add images component and rating to product page

---
+++ app/product/[productId]/page.tsx
@@ -0,0 +1,35 @@
+import Images from "@/components/productPage/Images";
+import Rating from "@/components/productPage/Rating";
+import Link from "next/link";
+import React from "react";
+import { FaChevronRight } from "react-icons/fa6";
+
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+
+const page = ({ params }: Props) => {
+  return (
+    <main className="w-full lg:px-32 px-8 max-w-[110rem]">
+      <div className="text-xs flex items-center gap-1 text-gray-600 py-6">
+        <Link href={"/"}>InnoTech</Link>
+        <FaChevronRight />
+        <Link href={"/category/28-processors"}>Processors</Link>
+      </div>
+      <div className="flex gap-2 flex-col md:flex-row">
+        <Images />
+        <div>
+          <h1 className="text-3xl">AMD Ryzen 7 7800X3D</h1>
+          <Rating rating={5.5} />
+          <p className="text-sm text-gray-600">
+            product id: {params.productId}
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default page;
